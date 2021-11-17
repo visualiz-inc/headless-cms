6e7cc0cd574d2609fc3aaa4219b3590019a26d00
rempei-okada <renpei.okada@outlook.com>
2021-11-17 21:40:55 +0900

    remove lf

--- shell.ps1
+++ shell.ps1
@@ -3,12 +3,9 @@ cd ./maccha/maccha.client
 yarn install
 yarn run build
 cp -r -fo ./public ../../
-
 cd ../maccha.server
 yarn install
 yarn run build
-
 cd ../../
-
 yarn install
 rm -fo -r maccha
